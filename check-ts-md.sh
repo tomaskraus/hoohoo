56a44f343710bee22fcb98e588d9229ffd037294
Tomas Kraus <tomas.kraus.dev@gmail.com>
2024-09-01 22:09:49 +0200

    fix(ts example script): incorrect jsDir path

--- check-ts-md.sh
+++ check-ts-md.sh
@@ -3,4 +3,4 @@
 rm -r dist
 npx hoohoo extract examples/example-1.md -l ts
 echo "- compile ----------------------------"
-npx tsc && npx hoohoo check examples/example-1.md -l ts --jsDir dist/example-1_hh
+npx tsc && npx hoohoo check examples/example-1.md -l ts --jsDir dist/examples/example-1_hh
